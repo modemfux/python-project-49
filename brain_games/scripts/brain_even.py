6ec731ed52f96af4fe5f515c4aeadb944a9bd45e
modemfux <modemfux@gmail.com>
2022-11-10 18:48:36 +0300

    Moved "even" game logic file to "games" folder, modified import functions from "games" folder

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from brain_games.cli import welcome_user
-from brain_games.even_game_logic import brain_even_game
+from brain_games.games.even_game_logic import brain_even_game
 
 
 def main():
