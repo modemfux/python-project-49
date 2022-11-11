2702da2ae643010c0bffdfc00097200f24a02ef3
modemfux <modemfux@gmail.com>
2022-11-11 21:32:32 +0300

    Added progression game logic and main script, modified main_game script to be able to call progression game

---
+++ brain_games/games/brain_progression_logic.py
@@ -0,0 +1,16 @@
+import random
+
+
+def progression_logic(start, step, length):
+    prog_list = []
+    i = 0
+    member = start
+    while i < length:
+        prog_list.append(str(member))
+        member += step
+        i += 1
+    index = random.randint(0, length - 1)
+    correct_answer = str(prog_list[index])
+    prog_list[index] = '..'
+    question = ' '.join(prog_list)
+    return (question, correct_answer)
--- brain_games/games/main_game_logic.py
+++ brain_games/games/main_game_logic.py
@@ -2,6 +2,7 @@ import random
 from brain_games.games.brain_calc_logic import calc_logic
 from brain_games.games.brain_even_logic import even_logic
 from brain_games.games.brain_gcd_logic import gcd_logic
+from brain_games.games.brain_progression_logic import progression_logic
 
 
 def main_game(game, name):
@@ -16,6 +17,9 @@ def main_game(game, name):
             game_question, correct_answer = even_logic(num1)
         elif game == 'gcd':
             game_question, correct_answer = gcd_logic(num1, num2)
+        elif game == 'progression':
+            length = random.randint(5, 15)
+            game_question, correct_answer = progression_logic(num1, num2, length)
         print(f'Question: {game_question}')
         answer = input('Your answer: ')
         if answer == correct_answer:
