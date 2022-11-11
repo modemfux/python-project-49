5f2bf79c17220031b6f276b82347aeefcd3459ee
modemfux <modemfux@gmail.com>
2022-11-11 16:41:28 +0300

    Renamed game files to one style

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from brain_games.cli import welcome_user
-from brain_games.games.brain_calc import brain_calc
+from brain_games.games.brain_calc_logic import brain_calc
 
 
 def main():
